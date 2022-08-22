700eca29e120d627689bc4dac5465a92d1070694
Tien Foong <46518750+breaddog@users.noreply.github.com>
2022-08-22 18:40:29 +1000

    change tests

--- contracts/CWTToken.sol
+++ contracts/CWTToken.sol
@@ -5,7 +5,6 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CWTToken is Ownable, ERC20 {
-  // 18 decimals
   uint256 public constant MAX_SUPPLY = 140000000000000000000000000;
 
   constructor() ERC20("CWT", "CWT") {
