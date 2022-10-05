0547bb1c96776681586519e134866de737aaab90
Tien Foong <46518750+breaddog@users.noreply.github.com>
2022-10-05 15:36:02 +1100

    tweak: tidying up

--- contracts/CWTToken.sol
+++ contracts/CWTToken.sol
@@ -6,10 +6,10 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // VUCA + Pellar + LightLink 2022
 
-contract CWTToken is Ownable, ERC20 {
-  uint256 public constant MAX_SUPPLY = 140000000;
+contract CrownToken is Ownable, ERC20 {
+  uint256 public constant MAX_SUPPLY = 140000000; // max supply of tokens (hard cap)
 
-  constructor() ERC20("CWT", "CWT") {
+  constructor() ERC20("CROWN", "CROWN") {
     _mint(msg.sender, MAX_SUPPLY * (10**decimals()));
   }
 }
